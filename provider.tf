155f2eff94084d023045d9b2ba1554d444eaba58
dquiros44 <azyrox144@gmail.com>
2025-11-20 17:01:25 -0600

    modified:   provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,15 @@
-hola
+terraform {
+
+    required_version = ">= 0.13.0"
+
+    required_providers {
+        discord = {
+            source = "Chaotic-Logic/discord"
+            version = "0.0.1"
+        }
+    }
+}
+
+provider "discord" {
+    token = var.discord_token
+}
