84ca6b8b37bde38dadb52efa50a026a996fc1d06
dquiros44 <111067341+dquiros44@users.noreply.github.com>
2025-11-20 20:21:27 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,11 @@
 # variables.tf
+# Input: Your server's ID from Step 1
+variable "server_id" {
+  type        = string
+  description = "ID of the existing Discord server to manage"
+  # Example: "123456789012345678"
+}
+
 variable "discord_token" {
   type      = string
   sensitive = true
